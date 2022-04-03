c4256861593aa9ce0fe4ce7b5710577eaefccb01
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-03 15:03:09 +0430

    add Clinic

--- src/main/java/model/entity/Clinic.java
+++ src/main/java/model/entity/Clinic.java
@@ -1,18 +1,43 @@
 package model.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import model.entity.base.BaseEntity;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@SuperBuilder(toBuilder = true)
 @Entity
-public class Clinic {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+public class Clinic extends BaseEntity<Integer> {
     private String name;
+
+    @Override
+    public String toString() {
+        return "Clinic{" +
+                "id='" + super.getId() + '\'' +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Clinic clinic = (Clinic) o;
+        return getId() != null && Objects.equals(getId(), clinic.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
