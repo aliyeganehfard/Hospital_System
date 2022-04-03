2b07d733b0453d9d538ec5fb9c0c7b688a9f7e80
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-03 15:01:08 +0430

    add BaseEntity

---
+++ src/main/java/model/entity/base/BaseEntity.java
@@ -0,0 +1,42 @@
+package model.entity.base;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.Hibernate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@SuperBuilder(toBuilder = true)
+@MappedSuperclass
+@NoArgsConstructor
+public class BaseEntity<ID extends Serializable> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private ID id;
+
+
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        BaseEntity<?> that = (BaseEntity<?>) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
