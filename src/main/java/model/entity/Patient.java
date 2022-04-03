2a094c38618fedffce5112c5256cfc3c4fda8d73
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-03 15:03:47 +0430

    add Patient

--- src/main/java/model/entity/Patient.java
+++ src/main/java/model/entity/Patient.java
@@ -1,21 +1,33 @@
 package model.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import model.entity.base.User;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
-import java.util.Set;
+import javax.persistence.Entity;
+import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
 @ToString
 @Entity
-public class Patient {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String username;
-    private String password;
-    private String name;
+public class Patient extends User {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Patient patient = (Patient) o;
+        return getId() != null && Objects.equals(getId(), patient.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
