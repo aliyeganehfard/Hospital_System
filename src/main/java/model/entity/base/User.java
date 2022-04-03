412276d85a1947a2b7b962001b570ae72be12991
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-03 15:02:48 +0430

    add User

---
+++ src/main/java/model/entity/base/User.java
@@ -0,0 +1,36 @@
+package model.entity.base;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type" , discriminatorType = DiscriminatorType.INTEGER)
+public class User extends BaseEntity<Integer>{
+    private String username;
+    private String password;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), username, password, name);
+    }
+}
