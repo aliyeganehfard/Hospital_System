e5f4a579f854ce9a4cc81bf8e86e34f5d5c46478
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-03 15:06:08 +0430

    add DoctorRepositoryImpl

--- src/main/java/model/repository/impl/DoctorRepositoryImpl.java
+++ src/main/java/model/repository/impl/DoctorRepositoryImpl.java
@@ -1,7 +1,7 @@
 package model.repository.impl;
 
 import model.entity.Doctor;
+import model.repository.LoginBase;
 
-public class DoctorRepositoryImpl extends RepositoryImpl<Doctor,Integer> {
-
+public class DoctorRepositoryImpl extends RepositoryImpl<Doctor,Integer> implements LoginBase<Doctor> {
 }
